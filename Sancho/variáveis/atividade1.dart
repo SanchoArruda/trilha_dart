cb85c0fcb0dacdd1bdaa02bb49ef6442a6ab5be0
SanchoArruda <gucairesarruda@gmail.com>
2025-09-07 12:28:57 -0300

    add: atv1

---
+++ Sancho/variáveis/atividade1.dart
@@ -0,0 +1,18 @@
+// Crie variáveis para armazenar:
+//- Seu nome
+//- Sua idade
+//- Sua altura
+//- Se você é estudante ou não
+//Depois, use `print()` para exibir cada uma no console.
+
+void main(){
+    String nome = 'Sancho';
+    int idade = 21;
+    double altura = 1.85;
+    bool estudante = true;
+
+    print("Nome: $nome");
+    print("Idade: $idade");
+    print("altura: $altura");
+    print("Estuda: $estudante");
+} 
